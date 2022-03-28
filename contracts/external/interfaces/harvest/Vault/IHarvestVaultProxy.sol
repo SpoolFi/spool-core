f2fafae9a0f8a6ae56a3268556ad90f1afbfcfdd
SpoolFi <spoolfi@protonmail.com>
2022-03-29 01:16:01 +0200

    cosmetics: remove redundant/unused interfaces

--- contracts/external/interfaces/harvest/Vault/IHarvestVaultProxy.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.11;
-
-interface IHarvestVaultProxy {
-    function implementation() external view returns (address);
-
-    function upgrade() external;
-}
